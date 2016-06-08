211ef459dcbb4546632748601e8ef232b1a528be
Ashwinss91 <ashwinss23@gmail.com>
2016-06-08 10:52:45 +0530

    Revert "AddedNewModels"
    This reverts commit 53cb82c9c35aa66848ec46385b458a303bfd4785.

--- EnquiryManagementSystem/Controllers/ValuesController.cs
+++ EnquiryManagementSystem/Controllers/ValuesController.cs
@@ -13,7 +13,6 @@ namespace EnquiryManagementSystem.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            var identity = this.User.Identity;
             return new string[] { "value1", "value2" };
         }
 
